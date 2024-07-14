b7ccee1648c27772a125e8c2c62431aa64302ba3
poa00 <poa0@icloud.com>
2024-07-14 14:33:26 -0500

    Update Addin.cs

--- src/TocAddin/Addin.cs
+++ src/TocAddin/Addin.cs
@@ -93,8 +93,7 @@ namespace TocAddin
 			int caretLineNumber = editor.GetLineNumber();
 
 			var markdown = GetMarkdown();
-			string[] lines = markdown.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-
+			string[] lines = markdown.Split(new string[] { "\n" }, StringSplitOptions.None); 
 			int maxDepth = TocAddinConfiguration.Current.MaxDepth;
 
 			if (maxDepth < 0)
